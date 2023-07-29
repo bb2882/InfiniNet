6b2c512c9a5e4c5de14ed01bfdee33252f2ef88a
Azat <harutyunyanazat29@gmail.com>
2023-07-29 19:05:44 +0400

    header elements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
 import Login from './components/Login';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
 	return (
 		<div className="App">
 			<BrowserRouter>
 				<Routes>
-					<Route path='/' element={<Login />} />
+					<Route path='/' element={ <Login /> } />
+					<Route path='/home' element= {
+						<>
+							<Header />
+							<Home />
+						</>
+					} />
 				</Routes>
 			</BrowserRouter>
 		</div>
