2d2daa10389e7ee8f54a4091e32d390a145e7b6f
Azat <harutyunyanazat29@gmail.com>
2023-08-02 17:20:18 +0400

    article template

--- src/components/Main.js
+++ src/components/Main.js
@@ -33,6 +33,72 @@ const Main = (props) => {
 				</div>
 
 			</ShareBox>
+
+			<div>
+				<Article>
+					<SharedActor>
+						<a>
+							<img src="/images/user.svg" alt="user" />
+							<div>
+								<span>Title</span>
+								<span>Info</span>
+								<span>Date</span>
+							</div>
+						</a>
+						<button>
+							<img src="/images/ellipsis.svg" alt="ellipsis" />
+						</button>
+					</SharedActor>
+
+					<Description>Description</Description>
+
+					<SharedImg>
+						<a>
+							<img src="/images/shared.jpg" alt="shared" />
+						</a>
+					</SharedImg>
+
+					<SocialCounts>
+						<li>
+							<button>
+								<img src="/images/like-icon.png" alt="like" />
+							
+								<img src="/images/clap-icon.png" alt="" />
+
+								<span>75</span>
+							</button>
+						</li>
+						<li>
+							<a>2 comments</a>
+						</li>
+					</SocialCounts>
+
+					<SocialActions>
+
+						<button>
+							<img src="/images/like-icon.png" alt="like" />
+							<span>Like</span>
+						</button>
+
+						<button>
+							<img src="/images/comments-icon.png" alt="" />
+							<span>Comments</span>
+						</button>
+
+						<button>
+							<img src="/images/share-icon.png" alt="" />
+							<span>Share</span>
+						</button>
+
+						<button>
+							<img src="/images/send-icon.png" alt="" />
+							<span>Send</span>
+						</button>
+
+					</SocialActions>
+				</Article>
+			</div>
+
 		</Container>
 	)
 }
@@ -110,6 +176,140 @@ const ShareBox = styled(CommonCard)`
 					color: #70b5f9;
 				}
 			}
+		}
+	}
+`;
+
+const Article = styled(CommonCard)`
+	padding: 0;
+	margin: 0 0 8px;
+	overflow: visible;
+`;
+
+const SharedActor = styled.div`
+	flex-wrap: nowrap;
+	padding: 12px 16px 0;
+	margin-bottom: 8px;
+	align-items: center;
+	display: flex;
+
+	a {
+		margin-right: 12px;
+		flex-grow: 1;
+		overflow: hidden;
+		display: flex;
+		text-decoration: none;
+
+		img {
+			width: 48px;
+			height: 48px;
+		}
+
+		& > div {
+			display: flex;
+			flex-direction: column;
+			flex-grow: 1;
+			flex-basis: 0;
+			margin-left: 8px;
+			overflow: hidden;
+
+			span {
+				text-align: left;
+
+				&:first-child {
+					font-size: 14px;
+					font-weight: 700;
+					color: rgba(0, 0, 0, 1);
+				}
+
+				&:nth-child(n+1) {
+					font-size: 12px;
+					color: rgba(0, 0, 0, .6);
+				}
+			}
+		}
+	}
+
+	button {
+		position: absolute;
+		right: 12px;
+		top: 5px;
+		background: transparent;
+		border: none;
+		outline: none;
+	}
+`;
+
+const Description = styled.div`
+	padding: 0 16px;
+	overflow: hidden;
+	color: rgba(0, 0, 0, .9);
+	font-size: 14px;
+	text-align: left;
+`;
+
+const SharedImg = styled.div`
+	margin-top: 8px;
+	width: 100%;
+	display: block;
+	position: relative;
+	background-color: #f9fafb;
+
+	img {
+		object-fit: contain;
+		width: 100%;
+		height: 100%;
+	}
+`;
+
+const SocialCounts = styled.ul`
+	line-height: 1.3;
+	display: flex;
+	align-items: flex-start;
+	overflow: auto;
+	margin: 0 16px;
+	padding: 8px 0;
+	border-bottom: 1px solid #e9e5df;
+	list-style: none;
+
+	li {
+		margin-right: 5px;
+		font-size: 12px;
+
+		button {
+			display: flex;
+
+			img {
+				width: 16px;
+				height: 16px;
+			}
+		}
+	}
+`;
+
+const SocialActions = styled.div`
+	align-items: center;
+	display: flex;
+	justify-content: flex-start;
+	margin: 0;
+	min-height: 40px;
+	padding: 4px 8px;
+
+	& > button {
+		display: inline-flex;
+		align-items: center;
+		padding: 8px;
+		color: #0a66c2;
+
+		@media (min-width: 768px) {
+			span {
+				margin-left: 8px;
+			}
+		}
+
+		img {
+			width: 24px;
+			height: 24px;
 		}
 	}
 `;
